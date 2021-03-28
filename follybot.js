99a3958dcf45902b630f0ffddc1398cabe97e611
Giuliano Ranauro <giulianoranauroiphone1@gmail.com>
2021-03-28 20:32:59 +0200

    token

--- follybot.js
+++ follybot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 //TOKEN da nascondere
-client.login('ODI1Nzk3MTE3NzY2NjY0MjAy.YGDJfw.KY9UK6Y_O9C4sRK0BRNcxMs5DAc');
+client.login('TOKEN HERE');
 
 client.on('ready',readyDiscord);
 

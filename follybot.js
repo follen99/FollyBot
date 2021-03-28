ff54bd2a39a32701c07057e8ce7e027017d2bd93
Giuliano Ranauro <giulianoranauroiphone1@gmail.com>
2021-03-28 20:32:03 +0200

    first structure

--- follybot.js
+++ follybot.js
@@ -1,4 +1,11 @@
-console.log("ciaone");
-//test
-//test2
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
+//TOKEN da nascondere
+client.login('ODI1Nzk3MTE3NzY2NjY0MjAy.YGDJfw.KY9UK6Y_O9C4sRK0BRNcxMs5DAc');
+
+client.on('ready',readyDiscord);
+
+function readyDiscord(){
+    console.log("Il bot è online!");
+}
